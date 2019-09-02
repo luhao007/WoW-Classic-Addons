768533792197e9abe9c7a3c319f47d357708205e
Peldir Sunstrider <luhao007@gmail.com>
2019-09-02 17:28:18 -0400

    Handle tocs

---
+++ toc.py
@@ -0,0 +1,33 @@
+class TOC(object):
+
+    def __init__(self, lines):
+        self.tags = dict(l[3:].split(':', 1) for l in lines if l.startswith('## ') and ':' in l)
+        self.contents = [l for l in lines if not l.startswith('## ')]
+        for i, e in enumerate(self.contents):
+            if e != '\n':
+                self.contents = self.contents[i:]
+                break
+
+    def tags_to_line(self, tags):
+        return ['## {}: {}\n'.format(tag, self.tags[tag].strip())
+                for tag in tags if tag in self.tags]
+
+    def to_lines(self):
+        keys = ['Interface', 'Author', 'Version',
+                'RequiredDeps', 'Dependencies', 'OptionalDeps',
+                'LoadOnDemand', 'LoadWith', 'LoadManagers',
+                'SavedVariables', 'SavedVariablesPerCharacter',
+                'DefaultState', 'Secure']
+        ret = self.tags_to_line(keys)
+        ret += ['\n']
+
+        keys = ['Title', 'Notes', 'Title-zhCN', 'Notes-zhCN']
+        ret += self.tags_to_line(keys)
+        ret += ['\n']
+
+        ret += self.tags_to_line(k for k in self.tags if k.startswith('X-'))
+        ret += ['\n']
+
+        ret += self.contents
+
+        return ret
