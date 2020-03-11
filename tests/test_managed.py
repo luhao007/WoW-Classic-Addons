1fae5db59dc665b8143e41289aba0c66db656ded
luhao007 <luhao007@gmail.com>
2020-03-12 01:00:23 +0900

    Convert \ to / for linux purpose

--- tests/test_managed.py
+++ tests/test_managed.py
@@ -35,7 +35,7 @@ class CheckManagedAddOns(unittest.TestCase):
                 continue
             path = root / line.strip()
             self.assertTrue(
-                os.path.exists(path),
+                os.path.exists(path.replace('\\', '/', -1)),
                 '{0} in !!Libs.toc, but not exists!'.format(path)
             )
 
