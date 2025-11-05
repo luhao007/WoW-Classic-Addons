5d3f2e66f0a3f32faf3f78b61b243d11b544006f
luhao007 <luhao007@gmail.com>
2025-11-05 21:41:21 +0900

    Update AddOns

--- AddOns/Leatrix_Plus/Leatrix_Plus.lua
+++ AddOns/Leatrix_Plus/Leatrix_Plus.lua
@@ -1,5 +1,5 @@
 ﻿----------------------------------------------------------------------
--- 	Leatrix Plus 5.1.07 (29th October 2025)
+-- 	Leatrix Plus 5.1.08 (5th November 2025)
 ----------------------------------------------------------------------
 
 --	01:Functions 02:Locks   03:Restart 40:Player   45:Rest
@@ -19,7 +19,7 @@
 	local void
 
 	-- Version
-	LeaPlusLC["AddonVer"] = "5.1.07"
+	LeaPlusLC["AddonVer"] = "5.1.08"
 
 	-- Get locale table
 	local void, Leatrix_Plus = ...
